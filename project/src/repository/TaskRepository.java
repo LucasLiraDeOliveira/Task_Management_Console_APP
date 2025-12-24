611d3867ded38f1d44e5a736cfa8e75b012e7913
Lucas Oliveira <lucas.oliveira845@gmail.com>
2025-12-24 15:45:39 -0300

    created TaskRepository structure

--- project/src/repository/TaskRepository.java
+++ project/src/repository/TaskRepository.java
@@ -1,4 +1,23 @@
 package repository;
 
+import model.Task;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class TaskRepository {
+    Set<Task> taskSet = new HashSet<>();
+
+    public void add(Task task){
+        taskSet.add(task);
+    }
+
+    public void remove(Task task){
+        taskSet.remove(task);
+    }
+
+    public List<Task> findAll(){
+        return List.copyOf(taskSet);
+    }
 }
