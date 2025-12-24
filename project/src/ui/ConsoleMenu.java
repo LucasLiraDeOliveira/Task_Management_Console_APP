44a8953dc34b1397d22b4f5bf5eea6655d6a73f6
Lucas Oliveira <lucas.oliveira845@gmail.com>
2025-12-24 16:21:35 -0300

    ConsoleMenu basic constructor added

--- project/src/ui/ConsoleMenu.java
+++ project/src/ui/ConsoleMenu.java
@@ -1,4 +1,12 @@
 package ui;
 
+import service.TaskService;
+
 public class ConsoleMenu {
+    private TaskService service;
+
+
+    public ConsoleMenu(TaskService taskService) {
+        this.service = taskService;
+    }
 }
