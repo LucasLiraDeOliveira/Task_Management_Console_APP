f4de5ae6f9f2b04468271fdefa738b2fc37133e2
Lucas Oliveira <lucas.oliveira845@gmail.com>
2025-12-24 15:48:10 -0300

    TaskService basic constructor added

--- project/src/service/TaskService.java
+++ project/src/service/TaskService.java
@@ -1,4 +1,12 @@
 package service;
 
+import repository.TaskRepository;
+
 public class TaskService {
+    TaskRepository repository;
+
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
 }
